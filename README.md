5a609e28900765f292e1a4a1971c032d02630257
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2021-09-19 15:45:11 +0100

    dropdown menu selecting eps but not displaying

--- README.md
+++ README.md
@@ -11,3 +11,21 @@ https://syllabus.codeyourfuture.io/js-core-3/tv-show-dom-project/readme
 The solution to this project will be given to you after you have completed it. You will be able to find the solution here
 
 https://github.com/CodeYourFuture/tv-show-dom-project-solution
+
+function episodeSelectorMenu(episodeList) {
+const selectBox = document.getElementById("selectBox");
+const selectEpisode = document.createElement("select");
+selectBox.appendChild(selectEpisode);
+selectEpisode.addEventListener("change", selectFilter);
+episodeList.map((episode) => {
+const episodeOptions = document.createElement("option");
+console.log(episodeOptions);
+selectEpisode.appendChild(episodeOptions);
+episodeOptions.innerText = `${episode.name} - S${episode.season}E${episode.number}`;
+});
+function selectFilter() {
+const usersOptionValue = document.querySelector("select");
+var selectedValue = usersOptionValue.value;
+const filterSelectedEpisode = allEpisodes.filter((episode) => {
+return episode.name.includes(selectedValue);
+});
--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 //You can edit ALL of the code here
 
 "use strict";
-
+const allEpisodes = getAllEpisodes();
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  const searchBar = document.getElementById("search");
-
-  searchBar.addEventListener("keyup", (e) => {
-    const searchEpisodes = e.target.value.toLowerCase();
-    const eachCard = document.getElementsByClassName("each-episode");
-
-    const filterEpisodes = allEpisodes.filter((episode) => {
-      return (
-        episode.name.toLowerCase().includes(searchEpisodes) ||
-        episode.summary.toLowerCase().includes(searchEpisodes)
-      );
-    });
-
-    Array.from(eachCard).forEach((e) => {
-      const title = e.textContent;
-      const episodeSummary = e.textContent;
-      if (
-        title.toLowerCase().indexOf(searchEpisodes) != -1 ||
-        episodeSummary.toLowerCase().indexOf(searchEpisodes) != -1
-      ) {
-        e.style.display = "block";
-      } else {
-        e.style.display = "none";
-      }
-    });
-  });
+  dropdownMenu(allEpisodes);
 }
 
+const searchBar = document.getElementById("search");
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchEpisodes = e.target.value.toLowerCase();
+  const eachCard = document.getElementsByClassName("each-episode");
+
+  // const filterEpisodes = allEpisodes.filter((episode) => {
+  //   return (
+  //     episode.name.toLowerCase().includes(searchEpisodes) ||
+  //     episode.summary.toLowerCase().includes(searchEpisodes)
+  //   );
+  // });
+
+  Array.from(eachCard).forEach((e) => {
+    const title = e.textContent;
+    //console.log(title);
+    const episodeSummary = e.textContent;
+    //console.log(episodeSummary);
+    if (
+      title.toLowerCase().indexOf(searchEpisodes) != -1 ||
+      episodeSummary.toLowerCase().indexOf(searchEpisodes) != -1
+    ) {
+      e.style.display = "block";
+    } else {
+      e.style.display = "none";
+    }
+  });
+});
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
@@ -58,7 +60,6 @@ function makePageForEpisodes(episodeList) {
     episodeImage.src = episode.image.medium;
 
     // episode code with season and number combined
-
     const epSeasonAndNum = document.createElement("h4");
     let season = episode.season.toString().padStart(2, 0);
     let episodeNum = episode.number.toString().padStart(2, 0);
@@ -71,6 +72,28 @@ function makePageForEpisodes(episodeList) {
     createCard.appendChild(episodeImage);
     createCard.appendChild(summaryText);
     episodeContainer.appendChild(createCard);
+  });
+}
+function dropdownMenu(episodeList) {
+  const selectEpisode = document.getElementById("selectEpisode");
+  selectEpisode.innerHTML = "";
+  episodeList.map((episode) => {
+    const seasonOption = ("" + episode.season).padStart(2, "0");
+    const episodeOption = ("" + episode.number).padStart(2, "0");
+    const wholeName = `S${seasonOption}E${episodeOption}`;
+    const option = document.createElement("option");
+    option.text = `${wholeName} - ${episode.name}`;
+    option.value = `${episode.name}`;
+    selectEpisode.appendChild(option);
+  });
+
+  selectEpisode.addEventListener("change", () => {
+    let filteredArray = allEpisodes.filter((episode) => {
+      let selectedValue = selectEpisode.value;
+      console.log(selectedValue);
+      return episode.name.includes(selectedValue);
+    });
+    makePageForEpisodes(filteredArray);
   });
 }
 
